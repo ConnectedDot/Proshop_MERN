50b1fc9556da1713d07f8420a42cc289156c9053
ConnectedDot <psalmomo@gmail.com>
2023-05-18 02:55:13 +0100

    Initials

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import './bootstrap.min.css';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          {/* <Routes> */}
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+          {/* </Routes> */}
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
