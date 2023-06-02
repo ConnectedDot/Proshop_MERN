9323bd8eaeed4f17a86d5edceb19ec4831312f59
ConnectedDot <psalmomo@gmail.com>
2023-06-02 14:04:23 +0100

    Pushing through the SideBar

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -13,7 +13,12 @@ const ProductCarousel = () => {
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
+            <Image
+              style={{ height: '550px' }}
+              src={product.image}
+              alt={product.name}
+              fluid
+            />
             <Carousel.Caption className="carousel-caption">
               <h2 className="text-white text-right">
                 {product.name} (${product.price})
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -67,7 +67,7 @@ const ProductScreen = () => {
 
   return (
     <>
-      <Link className="btn btn-dark my-3" to="/">
+      <Link className="btn btn-light my-3" to="/">
         Go Back
       </Link>
       {isLoading ? (
