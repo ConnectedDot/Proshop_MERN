8ddea6be07c9931bb5109cd43944c157df8c7433
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-10-15 08:55:47 +0100

    Added server URL at end

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,9 +1,13 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "develeopment"
-    ? "proshop-mern-backend.vercel.app"
-    : "";
+// export const BASE_URL =
+//   process.env.NODE_ENV === "develeopment"
+//     ? "proshop-mern-backend.vercel.app"
+//     : "";
 // export const BASE_URL = "proshop-mern-backend.vercel.app"; // If using proxy
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
 export const PAYPAL_URL = "/api/config/paypal";
+export const BASE_URL = "https://proshop-mern-backend.vercel.app";
+
+export const response = await fetch(`${BASE_URL}/products`);
+export const data = await response.json();
