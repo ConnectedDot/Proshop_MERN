90952077e9a9ede6557bf901bb2de7a31d8ba261
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2023-10-15 08:22:56 +0100

    Added server URL again

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,6 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL =
-  "proshop-mern-backend-s7q36axcd-connecteddot.vercel.app"; // If using proxy
+export const BASE_URL = "proshop-mern-backend.vercel.app"; // If using proxy
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
